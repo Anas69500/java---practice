f06b9c9838032bbeac5b7e8f93437f1dd2b81cd3
Mohammed Anas <anas11champ@gmail.com>
2025-10-18 19:09:09 +0530

    #

---
+++ practicequestion/reverseNumber.java
@@ -0,0 +1,31 @@
+package practicequestion;
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a two digit number: ");
+        while (true) {
+            
+            int num = sc.nextInt();
+            int rev = 0;
+
+            if (num > 9) {
+                while (num != 0) {
+                    int digit = num % 10;//it store reminder like from 1234 % 10 = 4
+
+                    rev = rev * 10 + digit;//0 * 10 + 4 = 4 in next loop 4 * 10 + 3 = 43 soon
+
+                    num = num / 10;// it will help to remove last digit from num like 1234 / 10 = 123.4 int only store 123
+                    System.out.print(rev);
+                }
+                break;
+                
+            } else {
+                System.out.print("please Enter a two digit number:");
+            }
+        }
+        
+    }
+}
---
+++ practicequestion/sumOfDigit.java
@@ -0,0 +1,19 @@
+package practicequestion;
+import java.util.Scanner;
+
+public class sumOfDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the a number to print from 1 to n: ");
+        int num = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= num; i++) {
+
+            sum += i;
+        }
+        System.out.println(sum);
+        
+    }
+}
