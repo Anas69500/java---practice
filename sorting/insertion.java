c6dad4c26a50a8190d15aa9f9a44fb5dd3b4a38d
Mohammed Anas <anas11champ@gmail.com>
2025-11-03 15:37:16 +0530

    #3/11/2025

--- sorting/insertion.java
+++ sorting/insertion.java
@@ -4,16 +4,18 @@ public class insertion {
     public static void main(String[] args) {
         int arr[] = {5,3,1,4,2};
         
+        //insertion sort
         for (int i = 0; i < arr.length; i++) {
-            int current = arr[i];
-            int j = i-1;
+            int current = arr[i]; //assign arr[i] value to to current variable
 
-            while (j >= 0 && current < arr[j]) {
-                arr[j+1] = arr[j];
-                j--;
+            int j = i-1; // this variable is use to track sorted part in array
+
+            while (j >= 0 && current < arr[j]) {// checking j greater than to 0 and checking current variable less than to arr[j]
+                arr[j+1] = arr[j];//and assign arr[j] value to arr[j+1]
+                j--;// and decresing j value -1 with post decrement
             }
 
-            arr[j+1] = current;
+            arr[j+1] = current;//and assign current value to arr[j+1]
             
         }
         for (int i = 0; i < arr.length; i++) {
